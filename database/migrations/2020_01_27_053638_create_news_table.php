7a12c40f44978838ebad2a79776cf30be8e31da4
shahil singh <yellowbananna@outlook.com>
2020-01-29 11:55:14 +0545

    news and migration

---
+++ database/migrations/2020_01_27_053638_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->longText('headline')->nullable();
+            $table->longText('content')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('status')->default(0);
+            $table->string('author')->nullable();
+            $table->longText('url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
